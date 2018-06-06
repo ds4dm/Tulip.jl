09ba1d74d7ee925d49b9b95f10e59c9997a694ac
mtanneau <mathieu.tanneau@gmail.com>
2018-06-06 14:23:56 -0400

    rename test file

---
+++ test/mathprogbase.jl
@@ -0,0 +1,33 @@
+import MathProgBase
+
+const MPB = MathProgBase
+
+solver_tlp = Tulip.TulipSolver(output_level=0, time_limit=10)
+
+m, n, c, b, A, collb, colub, ranges = Tulip.readmps(testdir*"/../dat/netlib/AFIRO.SIF");
+
+# instanciate model
+model_tlp = MPB.LinearQuadraticModel(solver_tlp)
+@test typeof(model_tlp) <: MPB.AbstractLinearQuadraticModel
+
+MPB.loadproblem!(model_tlp, A, collb, colub, c, b, b, :Min)
+
+@test MPB.getvarLB(model_tlp) == collb
+@test MPB.getvarUB(model_tlp) == colub
+@test MPB.getobj(model_tlp) == c
+@test MPB.getconstrmatrix(model_tlp) == A
+
+
+MPB.optimize!(model_tlp)
+
+
+# get solution
+
+
+# # TODO: Run MathProgBase tests instead
+# solver_tlp = Tulip.TulipSolver(output_level=0, time_limit=100.0)
+# include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
+# linprogtest(solver_tlp)
+# MathProgBase.setparameters!(solver_tlp, Silent=true, TimeLimit=100.0)
+# include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
+# linprogsolvertest(solver_tlp)
