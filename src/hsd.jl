21e93079226643123f083d2e0649024cceb1fdca
Tanneau Mathieu <mathieu.tanneau@gmail.com>
2018-10-15 10:02:01 -0400

    HSD step squeleton

--- src/hsd.jl
+++ src/hsd.jl
@@ -2,16 +2,86 @@
     make_step!()
 
 Compute next IP iterate for the HSD formulation.
+
+# Arguments
+- `model`: The optimization model
+- `env`: Optimization environment
+- `A`: Constraint matrix
+- `F`: Factorization of the normal equations' matrix
+- `b`: Right-hand side of primal constraints
+- `c`: Primal linear objective term
+- `uind, uval`: Indices and values of variables' upperbounds
+- `θ, θwz`: Diagonal scaling terms
+- `rp, ru, rd, rg`: Primal, dual and optimality residuals
+- `x, w, y, s, z, t, k`: Primal-dual iterate
 """
 function make_step!(
     ::Val{1},
-    model, env,
+    model::Model, env::TulipEnv,
     A, F, b, c, uind, uval,
     θ, θwz,
-    x, w, y, s, z, t, k
+    rp, ru, rd, rg::RefValue,
+    x, w, y, s, z, t::RefValue, k::RefValue
 )
-    # Affine-scaling direction
 
+    # Compute p, q, r, ρ from augmented system
+    p = zeros(model.num_var)
+    q = zeros(model.num_constr)
+    r = zeros(model.n_var_ub)
+    solve_augsys_hsd!(
+        A, F, θ, θwz, uind,
+        p, q, r,
+        b, c, uval
+    )
+    ρ = (k.x / t.x) - dot(c, p) + dot(b, q) - dot(uval, r)
+
+    # Affine-scaling direction
+    solve_newton_hsd!(
+        A, F, b, c, uind, uval, θ, θwz,
+        p, q, r, ρ,
+        x, w, y, s, z, t, k,
+        dx, dw, dy, ds, dz, dt, dk,
+        # Right-hand side of Newton system
+        rp, ru, rd, rg.x,
+        -x  .* s,
+        -w  .* z,
+        -t.x * k.x
+    )
+
+    # Step length for affine-scaling direction
+    γ = (1-a)^2 * min(1-a, beta1)
+    η = 1.0 - γ
+    
+    # Mehrothra corrector
+    solve_newton_hsd!(
+        A, F, b, c, uind, uval, θ, θwz,
+        p, q, r, ρ,
+        x, w, y, s, z, t, k,
+        dx, dw, dy, ds, dz, dt, dk,
+        # Right-hand side of Newton system
+        η .* rp, η .* ru, η .* rd, η * rg.x,
+        -x  .* s   .+ γ * μ.x .- dx  .* ds,
+        -w  .* z   .+ γ * μ.x .- dw  .* dz,
+        -t.x * k.x  + γ * μ.x  - dt.x * dk.x
+    )
+
+    # Extra corrections
+    ncor = 0
+    while ncor < env.barrier_max_num_cor.val
+        # TODO: Compute extra-corrector
+        ncor += 1
+    end
+
+
+    x  .+= α .* dx
+    w  .+= α .* dw
+    t.x += α  * dt.x
+    y  .+= α .* dy
+    s  .+= α .* ds
+    z  .+= α .* dz
+    k.x += α  * dk.x
+    
+    return nothing
 end
 
 """
@@ -46,7 +116,7 @@ function solve_newton_hsd!(
     A, F, b, c, uind, uval, θ, θwz,
     p, q, r, ρ,
     x, w, y, s, z, t, k,
-    dx, dw, dy, ds, dz, dt, dk,
+    dx, dw, dy, ds, dz, dt::RefValue, dk::RefValue,
     rp, ru, rd, rg::Float64, rxs, rwz, rtk::Float64
 )
     # Solve reduced newton system
