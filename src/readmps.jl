743e798212a06f018c4cc4d82922f40141454ae8
mtanneau <mathieu.tanneau@gmail.com>
2019-04-19 13:55:45 -0400

    Trim large bounds

--- src/readmps.jl
+++ src/readmps.jl
@@ -146,15 +146,23 @@ function convert_to_standard_form(
     obj = Array(sparsevec(obj_col, obj_val, n))
     rhs = Array(sparsevec(rhs_row, rhs_val, m))
 
-
+    @info "$(extrema(ub_col))"
     # II. Form bounds
     lb = zeros(n)  # lower bound
     ub = Inf * ones(n)  # upper bound
     for (i, j) in enumerate(lb_col)
-        lb[j] = lb_val[i]
+        if lb_val[i] < -1e16
+            lb[j] = -Inf
+        else
+            lb[j] = lb_val[i]
+        end
     end
     for (i, j) in enumerate(ub_col)
-        ub[j] = ub_val[i]
+        if ub_val[i] > 1e16
+            ub[j] = Inf
+        else
+            ub[j] = ub_val[i]
+        end
     end
 
     return m, n, obj, rhs, coeffs, lb, ub
