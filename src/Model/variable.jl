734f6f9ee81a4fb2ea6eac5cbb7cb3fa89ff1b87
mtanneau <mathieu.tanneau@gmail.com>
2019-08-05 11:18:04 -0400

    Remove rows field and make index immutable

--- src/Model/variable.jl
+++ src/Model/variable.jl
@@ -1,31 +1,54 @@
 """
-    Variable{Tv<:Real, Ti<:Integer}
+    VarId{Ti<:Integer}
 
-Place-holder for variables.
 """
-mutable struct Variable{Tv<:Real, Ti<:Integer}
-    uuid::Ti      # Unique identifier
-    name::String  # Variable name
-
-    obj::Tv  # Objective coeff
-
-    lb::Tv  # Lower bound
-    ub::Tv  # Upper bound
-
-    # Constructor
-    # TODO: check and add useful constructors
-    function Variable{Tv, Ti}(uuid, name, obj, lb, ub) where{Tv<:Real, Ti<:Integer}
-        return new{Tv, Ti}(uuid, name, obj, lb, ub)
-    end
+struct VarId{Ti<:Integer}
+    uuid::Ti  # Unique identifier
 end
 
 
 """
-    Variable{Tv, Ti}(uuid::Integer)
+    VarData{Tv<:Real, Ti<:Integer}
 
 """
-function Variable{Tv, Ti}(uuid::Integer) where{Tv<:Real, Ti<:Integer}
-    return Variable{Tv, Ti}(uuid, "", zero(Tv), typemin(Tv), typemax(Tv))
+mutable struct VarData{Tv<:Real}
+    name::String
+
+    obj::Tv  # Objective coeff
+    lb::Tv   # Lower bound
+    ub::Tv   # Upper bound
+
+    # Constructors
+    VarData{Tv}() where {Tv<:Real} = new{Tv}("", zero(Tv), typemin(Tv), typemax(Tv))
+    
+    function VarData{Tv}(name::String, obj, lb, ub) where {Tv<:Real}
+        return new{Tv}(name, obj, lb, ub)
+    end
+
+end
+
+VarData(name::String, obj::Tv, lb::Tv, ub::Tv) where{Tv<:Real} = VarData{Tv}(name, obj, lb, ub)
+
+
+"""
+    Variable{Tv<:Real, Ti<:Integer}
+
+Place-holder for variables.
+"""
+struct Variable{Tv<:Real, Ti<:Integer}
+    id::VarId{Ti}     # Unique identifier
+    dat::VarData{Tv}  # Variable data
+
+    # Constructor
+    function Variable(id::VarId{Ti}, dat::VarData{Tv}) where{Tv<:Real, Ti<:Integer}
+        return new{Tv, Ti}(id, dat)
+    end
+    Variable{Tv, Ti}(id::VarId{Ti}) where{Tv<:Real, Ti<:Integer} = Variable(id, VarData{Tv}())
+end
+
+function Variable{Tv, Ti}(id::VarId{Ti}, name::String, obj, lb, ub) where{Tv<:Real, Ti<:Integer}
+    vd = VarData{Tv}(name, obj, lb, ub)
+    return Variable(id, vd)
 end
 
 
@@ -34,7 +57,7 @@ end
 
 Return the identifier of variable `v`.
 """
-get_uuid(v::Variable) = v.uuid
+get_uuid(v::Variable) = v.id
 
 
 """
@@ -42,7 +65,7 @@ get_uuid(v::Variable) = v.uuid
 
 Return the name of variable `v`.
 """
-get_name(v::Variable) = v.name
+get_name(v::Variable) = v.dat.name
 
 
 """
@@ -50,7 +73,7 @@ get_name(v::Variable) = v.name
 
 Set the name of variable `v` to `s`.
 """
-set_name!(v::Variable, s::String) = (v.name = s)
+set_name!(v::Variable, s::String) = (v.dat.name = s)
 
 
 """
@@ -58,7 +81,7 @@ set_name!(v::Variable, s::String) = (v.name = s)
 
 Return the objective coefficient of variable `v`.
 """
-get_obj_coeff(v::Variable) = v.obj
+get_obj_coeff(v::Variable) = v.dat.obj
 
 
 """
@@ -66,55 +89,32 @@ get_obj_coeff(v::Variable) = v.obj
 
 Set objective coefficient of variable `v` to `c`.
 """
-set_obj_coeff!(v::Variable, coeff) = (v.obj = coeff)
+set_obj_coeff!(v::Variable, coeff) = (v.dat.obj = coeff)
 
 
 """
     get_lower_bound(v::Variable)
 
 """
-get_lower_bound(v::Variable) = v.lb
+get_lower_bound(v::Variable) = v.dat.lb
 
 
 """
     set_lower_bound(v::Variable, l)
 
 """
-set_lower_bound!(v::Variable, l) = (v.lb = l)
+set_lower_bound!(v::Variable, l) = (v.dat.lb = l)
 
 
 """
     get_upper_bound(v::Variable)
 
 """
-get_upper_bound(v::Variable) = v.ub
+get_upper_bound(v::Variable) = v.dat.ub
 
 
 """
     set_upper_bound(v::Variable, u)
 
 """
-set_upper_bound!(v::Variable, u) = (v.ub = u)
-
-
-"""
-    get_rows(v::Variable)
-
-"""
-get_rows(v::Variable) = copy(v.rows)
-
-
-"""
-    add_row(v::Variable, r)
-
-Add one row to variable `v`.
-"""
-add_row!(v::Variable, r) = push!(v.rows, r)
-
-
-"""
-    delete_row!(v::Variable, r)
-
-Delete row `r` from variable `v`.
-"""
-delete_row!(v::Variable, r) = error("delete_row! not implemented yet.")
+set_upper_bound!(v::Variable, u) = (v.dat.ub = u)
