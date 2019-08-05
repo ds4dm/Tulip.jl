8de0aa7f55bc8c347da469679b188c28328c8063
mtanneau <mathieu.tanneau@gmail.com>
2019-08-05 17:23:32 -0400

    Resolve namespace in tests

--- test/Model/constraint.jl
+++ test/Model/constraint.jl
@@ -1,60 +1,56 @@
-using Test
-
-include("../../src/Model/constraint.jl")
-
 # TODO: run this with multiple combinations of Tv types.
 # This may require to encapsulate each testset in its own typed function.
 # Tv types: Float32, Float64, BigFloat
 function run_tests_lincon()
     # Constructors
     @testset "Constructors" begin
-        cid = ConstrId(1)
-        cdat = LinConstrData{Float64}("c1", 0.0, Inf)
-        c = LinearConstraint(cid, cdat)
+        cid = TLP.ConstrId(1)
+        cdat = TLP.LinConstrData{Float64}("c1", 0.0, Inf)
+        c = TLP.LinearConstraint(cid, cdat)
 
-        c = LinearConstraint{Float64}(cid)
+        c = TLP.LinearConstraint{Float64}(cid)
 
-        c = LinearConstraint{Float64}(cid, "c1", 0.0, Inf)
+        c = TLP.LinearConstraint{Float64}(cid, "c1", 0.0, Inf)
     end
 
-    cid = ConstrId(1)
-    cdat = LinConstrData{Float64}("c1", 0.0, Inf)
-    c = LinearConstraint(cid, cdat)
+    cid = TLP.ConstrId(1)
+    cdat = TLP.LinConstrData{Float64}("c1", 0.0, Inf)
+    c = TLP.LinearConstraint(cid, cdat)
 
     # UUID
     @testset "UUID" begin
-        @test get_uuid(c) == cid
+        @test TLP.get_uuid(c) == cid
     end
 
     # Name
     @testset "Name" begin
-        @test get_name(c) == "c1"
+        @test TLP.get_name(c) == "c1"
 
-        set_name!(c, "c2")
-        @test get_name(c) == "c2"
+        TLP.set_name!(c, "c2")
+        @test TLP.get_name(c) == "c2"
     end
 
     # Bounds
     @testset "Bounds" begin
-        @test get_lower_bound(c) == 0.0
-        @test get_upper_bound(c) == Inf
+        @test TLP.get_lower_bound(c) == 0.0
+        @test TLP.get_upper_bound(c) == Inf
 
-        set_lower_bound!(c, -1.0)
-        set_upper_bound!(c,  1.0)
+        TLP.set_lower_bound!(c, -1.0)
+        TLP.set_upper_bound!(c,  1.0)
 
-        @test get_lower_bound(c) == -1.0
-        @inferred get_lower_bound(c)
-        @test get_upper_bound(c) ==  1.0
-        @inferred get_upper_bound(c)
+        @test TLP.get_lower_bound(c) == -1.0
+        @inferred TLP.get_lower_bound(c)
+        @test TLP.get_upper_bound(c) ==  1.0
+        @inferred TLP.get_upper_bound(c)
 
         # Check type conversion
-        set_lower_bound!(c, 0)
-        @test get_lower_bound(c) == 0.0
-        @inferred get_lower_bound(c)
+        TLP.set_lower_bound!(c, 0)
+        @test TLP.get_lower_bound(c) == 0.0
+        @inferred TLP.get_lower_bound(c)
 
-        set_upper_bound!(c, 2)
-        @test get_upper_bound(c) ==  2.0
-        @inferred get_upper_bound(c)
+        TLP.set_upper_bound!(c, 2)
+        @test TLP.get_upper_bound(c) ==  2.0
+        @inferred TLP.get_upper_bound(c)
     end
 
     return nothing
--- test/Model/variable.jl
+++ test/Model/variable.jl
@@ -1,75 +1,71 @@
-using Test
-
-include("../../src/Model/variable.jl")
-
 # TODO: run this with multiple combinations of Tv types.
 # This may require to encapsulate each testset in its own typed function.
 # Tv types: Float32, Float64, BigFloat
 function run_tests_variable()
     # Constructors
     @testset "Constructors" begin
-        vid = VarId(1)
-        vdat = VarData("x1", 0.0, 0.0, Inf)
-        v = Variable(vid, vdat)
+        vid = TLP.VarId(1)
+        vdat = TLP.VarData("x1", 0.0, 0.0, Inf)
+        v = TLP.Variable(vid, vdat)
 
-        v = Variable{Float64}(vid)
+        v = TLP.Variable{Float64}(vid)
 
-        v = Variable{Float64}(vid, "x1", 0.0, 0.0, Inf)
+        v = TLP.Variable{Float64}(vid, "x1", 0.0, 0.0, Inf)
     end
 
-    vid = VarId(1)
-    vdat = VarData("x1", 0.0, 0.0, Inf)
-    v = Variable(vid, vdat)
+    vid = TLP.VarId(1)
+    vdat = TLP.VarData("x1", 0.0, 0.0, Inf)
+    v = TLP.Variable(vid, vdat)
 
     # UUID
     @testset "UUID" begin
-        @test get_uuid(v) == vid
+        @test TLP.get_uuid(v) == vid
     end
 
     # Name
     @testset "Name" begin
-        @test get_name(v) == "x1"
+        @test TLP.get_name(v) == "x1"
 
-        set_name!(v, "x2")
-        @test get_name(v) == "x2"
+        TLP.set_name!(v, "x2")
+        @test TLP.get_name(v) == "x2"
     end
 
     # Objective coefficients
     @testset "Objective" begin
-        @test get_obj_coeff(v) == 0.0
+        @test TLP.get_obj_coeff(v) == 0.0
 
         # With initial type
-        set_obj_coeff!(v, 1.0)
-        @test get_obj_coeff(v) == 1.0
-        @inferred get_obj_coeff(v)
+        TLP.set_obj_coeff!(v, 1.0)
+        @test TLP.get_obj_coeff(v) == 1.0
+        @inferred TLP.get_obj_coeff(v)
 
         # Check type conversion
-        set_obj_coeff!(v, 0)
-        @inferred get_obj_coeff(v)
-        @test get_obj_coeff(v) == 0.0
+        TLP.set_obj_coeff!(v, 0)
+        @inferred TLP.get_obj_coeff(v)
+        @test TLP.get_obj_coeff(v) == 0.0
     end
 
     # Bounds
     @testset "Bounds" begin
-        @test get_lower_bound(v) == 0.0
-        @test get_upper_bound(v) == Inf
+        @test TLP.get_lower_bound(v) == 0.0
+        @test TLP.get_upper_bound(v) == Inf
 
-        set_lower_bound!(v, -1.0)
-        set_upper_bound!(v,  1.0)
+        TLP.set_lower_bound!(v, -1.0)
+        TLP.set_upper_bound!(v,  1.0)
 
-        @test get_lower_bound(v) == -1.0
-        @inferred get_lower_bound(v)
-        @test get_upper_bound(v) ==  1.0
-        @inferred get_upper_bound(v)
+        @test TLP.get_lower_bound(v) == -1.0
+        @inferred TLP.get_lower_bound(v)
+        @test TLP.get_upper_bound(v) ==  1.0
+        @inferred TLP.get_upper_bound(v)
 
         # Check type conversion
-        set_lower_bound!(v, 0)
-        @test get_lower_bound(v) == 0.0
-        @inferred get_lower_bound(v)
+        TLP.set_lower_bound!(v, 0)
+        @test TLP.get_lower_bound(v) == 0.0
+        @inferred TLP.get_lower_bound(v)
 
-        set_upper_bound!(v, 2)
-        @test get_upper_bound(v) ==  2.0
-        @inferred get_upper_bound(v)
+        TLP.set_upper_bound!(v, 2)
+        @test TLP.get_upper_bound(v) ==  2.0
+        @inferred TLP.get_upper_bound(v)
     end
 
     return nothing
