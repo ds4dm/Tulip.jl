7c190b11e2d2c7f35fb670548cc0240077191040
mtanneau <mathieu.tanneau@gmail.com>
2019-08-05 17:23:52 -0400

    Include unit tests on new code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,23 @@
-using Tulip
 using Test
 
 using LinearAlgebra
 using SparseArrays
+
+using Tulip
+TLP = Tulip
 
 # write your own tests here
 const testdir = dirname(@__FILE__)
 
 const test_files = [
     # include test file name here (without .jl extension)
+    "Model/model",
     "params",
     "env",
     "LinearAlgebra/linearAlgebra",
-    "hsd",
-    "model",
-    "mathprogbase",
+    # "hsd",
+    # "model",
+    # "mathprogbase",
 ]
 
 for f in test_files
