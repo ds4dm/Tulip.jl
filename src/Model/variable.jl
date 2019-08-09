0cf84ffa7a759fb9ec8656137c04b27e9212ee7e
mtanneau <mathieu.tanneau@gmail.com>
2019-08-09 14:53:34 -0400

    Move bound checking code to enums file

--- src/Model/variable.jl
+++ src/Model/variable.jl
@@ -129,22 +129,3 @@ function set_bounds!(v::Variable{Tv}, bt::BoundType, lb, ub) where{Tv<:Real}
 
     return nothing
 end
-
-
-function _check_bounds(bt::BoundType, lb::Tv, ub::Tv) where{Tv<:Real}
-    if bt == TLP_BND_FX
-        return Tv(-Inf)  < lb == ub  < Tv(Inf)
-    elseif bt == TLP_BND_RG
-        return Tv(-Inf)  < lb <= ub  < Tv(Inf)
-    elseif bt == TLP_BND_UP
-        return Tv(-Inf) == lb  < ub  < Tv(Inf)
-    elseif bt == TLP_BND_LO
-        return Tv(-Inf)  < lb  < ub == Tv(Inf)
-    elseif bt == TLP_BND_FR
-        return (Tv(-Inf) == lb) && (ub == Tv(Inf))
-    else
-        error("Unsupported bound type: $bt.")
-    end
-
-    return false
-end
--- src/enums.jl
+++ src/enums.jl
@@ -9,3 +9,22 @@
     TLP_BND_FR  # Free
     TLP_BND_RG  # Range
 end
+
+
+function _check_bounds(bt::BoundType, lb::Tv, ub::Tv) where{Tv<:Real}
+    if bt == TLP_BND_FX
+        return Tv(-Inf)  < lb == ub  < Tv(Inf)
+    elseif bt == TLP_BND_RG
+        return Tv(-Inf)  < lb <= ub  < Tv(Inf)
+    elseif bt == TLP_BND_UP
+        return Tv(-Inf) == lb  < ub  < Tv(Inf)
+    elseif bt == TLP_BND_LO
+        return Tv(-Inf)  < lb  < ub == Tv(Inf)
+    elseif bt == TLP_BND_FR
+        return (Tv(-Inf) == lb) && (ub == Tv(Inf))
+    else
+        error("Unsupported bound type: $bt.")
+    end
+
+    return false
+end
