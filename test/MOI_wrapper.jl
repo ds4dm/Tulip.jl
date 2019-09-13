09461b1306adfc4a15882f3160e6a853261e3fd4
Mathieu Tanneau <mathieu.tanneau@gmail.com>
2019-09-13 09:40:19 -0400

    Exclude tests that require VectorAffineFunction

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -8,14 +8,16 @@ const CONFIG = MOIT.TestConfig(basis=false)
 @testset "Unit Tests" begin
     MOIT.basic_constraint_tests(OPTIMIZER, CONFIG; delete=false)
     MOIT.unittest(OPTIMIZER, MOIT.TestConfig(atol=1e-6), [
-        "solve_integer_edge_cases",         # Requires integer variables
-        "solve_qcp_edge_cases",             # Requires quadratic constraints
-        "solve_qp_edge_cases",              # Requires quadratic objective
-        "solve_zero_one_with_bounds_1",     # Requires binary variables
-        "solve_zero_one_with_bounds_2",     # Requires binary variables
-        "solve_zero_one_with_bounds_3",     # Requires binary variables
-        "solve_affine_deletion_edge_cases", # Requires VectorAffineFunction-in-Nonpositives
-        "solve_objbound_edge_cases",         # Requires integer variables
+        "solve_integer_edge_cases",             # Requires integer variables
+        "solve_qcp_edge_cases",                 # Requires quadratic constraints
+        "solve_qp_edge_cases",                  # Requires quadratic objective
+        "solve_zero_one_with_bounds_1",         # Requires binary variables
+        "solve_zero_one_with_bounds_2",         # Requires binary variables
+        "solve_zero_one_with_bounds_3",         # Requires binary variables
+        "solve_affine_deletion_edge_cases",     # Requires VectorAffineFunction-in-Nonpositives
+        "solve_multirow_vectoraffine_nonpos",   # Requires VectorAffineFunction-in-Nonpositives
+        "solve_const_vectoraffine_nonpos",      # Requires VectorAffineFunction-in-Nonpositives
+        "solve_objbound_edge_cases",            # Requires integer variables
         "solve_duplicate_terms_vector_affine",  # Requires `VectorAffineFunction`
     ])
     MOIT.modificationtest(OPTIMIZER, CONFIG)
