cd90f48dc45519663d710920cd74d3f9302f6d91
mtanneau <9593025+mtanneau@users.noreply.github.com>
2020-08-15 17:58:27 -0400

    Fix index update after row/column deletion (#53)

--- test/Core/problemData.jl
+++ test/Core/problemData.jl
@@ -90,8 +90,57 @@ function run_tests_pbdata(Tv::Type)
 
     end
 
-    @testset "Modification" begin
+    @testset "Delete" begin
+        pb = TLP.ProblemData{Tv}("test")
+        #=
+            min     x1 + 2 x2 + 3 x3
+            s.t.    1 ⩽ 1 * x1 ⩽ 10
+                    2 ⩽ 2 * x2 ⩽ 20
+                    3 ⩽ 3 * x3 ⩽ 30
 
+                    11 ⩽ x1 ⩽ 110
+                    22 ⩽ x2 ⩽ 220
+                    33 ⩽ x3 ⩽ 330
+        =#
+
+        TLP.add_variable!(pb, Int[], Tv[],     one(Tv), 11 * one(Tv), 110 * one(Tv), "x1")
+        TLP.add_variable!(pb, Int[], Tv[], 2 * one(Tv), 22 * one(Tv), 220 * one(Tv), "x2")
+        TLP.add_variable!(pb, Int[], Tv[], 3 * one(Tv), 33 * one(Tv), 330 * one(Tv), "x3")
+
+        TLP.add_constraint!(pb, [1], Tv.([1]), 1 * one(Tv), 10 * one(Tv), "row1")
+        TLP.add_constraint!(pb, [2], Tv.([2]), 2 * one(Tv), 20 * one(Tv), "row2")
+        TLP.add_constraint!(pb, [3], Tv.([3]), 3 * one(Tv), 30 * one(Tv), "row3")
+
+        # Delete row 1 and check remaining problem
+        TLP.delete_constraint!(pb, 1)
+
+        @test pb.ncon == 2
+        @test pb.nvar == 3
+        row2, row3 = pb.arows
+        @test pb.con_names == ["row2", "row3"]
+        @test pb.lcon == Tv.([2, 3])
+        @test pb.ucon == Tv.([20, 30])
+
+        @test row2.nzind == [2]
+        @test row2.nzval == [Tv(2)]
+
+        @test row3.nzind == [3]
+        @test row3.nzval == [Tv(3)]
+
+        # Delete variable 2
+        TLP.delete_variable!(pb, 2)
+        @test pb.ncon == 2
+        @test pb.nvar == 2
+        col1, col3 = pb.acols
+        @test pb.var_names == ["x1", "x3"]
+        @test pb.lvar == Tv.([11, 33])
+        @test pb.uvar == Tv.([110, 330])
+
+        @test col1.nzind == []
+        @test col1.nzval == Tv[]
+
+        @test col3.nzind == [2]
+        @test col3.nzval == [Tv(3)]
     end
 
     @testset "Queries" begin
