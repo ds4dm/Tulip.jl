e93201552bbc7d17728ecbc7b31f2bcd81ca21c0
mtanneau <mathieu.tanneau@gmail.com>
2020-09-02 15:15:04 -0400

    Add unit tests for SQD solvers

--- test/KKTSolver/krylov.jl
+++ test/KKTSolver/krylov.jl
@@ -1,14 +1,28 @@
 import Krylov
 
 @testset "Krylov" begin
-    for T in TvTYPES, f in [Krylov.cg, Krylov.minres]
-        @testset "$f, $T" begin
-            A = SparseMatrixCSC{T, Int}([
-                1 0 1 0;
-                0 1 0 1
-            ])
-            kkt = KKT.KrylovPDSolver(f, A)
-            KKT.run_ls_tests(A, kkt)
+    for T in TvTYPES
+        @testset "$T" begin
+            for f in [Krylov.cg, Krylov.minres, Krylov.minres_qlp]
+                @testset "$f" begin
+                    A = SparseMatrixCSC{T, Int}([
+                        1 0 1 0;
+                        0 1 0 1
+                    ])
+                    kkt = KKT.KrylovPDSolver(f, A)
+                    KKT.run_ls_tests(A, kkt)
+                end
+            end
+            for f in [Krylov.tricg, Krylov.trimr]
+                @testset "$f" begin
+                    A = SparseMatrixCSC{T, Int}([
+                        1 0 1 0;
+                        0 1 0 1
+                    ])
+                    kkt = KKT.KrylovSQDSolver(f, A)
+                    KKT.run_ls_tests(A, kkt)
+                end
+            end
         end
     end
 end
