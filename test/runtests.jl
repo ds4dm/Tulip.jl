5ea060c392e0f76822703b8728970e9203ed48d1
mtanneau <mathieu.tanneau@gmail.com>
2020-09-24 22:48:41 -0400

    Update unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,34 +13,33 @@ const TvTYPES = [Float64, BigFloat]
 # write your own tests here
 const testdir = dirname(@__FILE__)
 
-@testset "Tulip tests" begin
-    @testset "Unit tests" begin
-        include("Core/problemData.jl")
-        include("Solvers/HSD.jl")
-    end
 
-    @testset "KKT" begin
-        include("KKTSolver/KKTSolver.jl")
-    end
+@testset "Unit tests" begin
+    include("Core/problemData.jl")
+    include("Solvers/HSD.jl")
+end
 
-    @testset "Presolve" begin
-        include("Presolve/presolve.jl")
-    end
+@testset "KKT" begin
+    include("KKTSolver/KKTSolver.jl")
+end
 
-    @testset "Interfaces" begin
-        include("Interfaces/MOI_wrapper.jl")
-    end
+@testset "Presolve" begin
+    include("Presolve/presolve.jl")
+end
 
-    @testset "Examples" begin
-        include("examples.jl")
-    end
+@testset "Examples" begin
+    include("examples.jl")
+end
 
-    @testset "Convex Problem Depot tests" begin
-        for Tv in TvTYPES
-            @testset "$Tv" begin
-                Convex.ProblemDepot.run_tests(;  exclude=[r"mip", r"exp", r"socp", r"sdp"], T = Tv) do problem
-                    Convex.solve!(problem, () -> Tulip.Optimizer{Tv}())
-                end
+@testset "Interfaces" begin
+    include("Interfaces/MOI_wrapper.jl")
+end
+
+@testset "Convex Problem Depot tests" begin
+    for T in TvTYPES
+        @testset "$T" begin
+            Convex.ProblemDepot.run_tests(;  exclude=[r"mip", r"exp", r"socp", r"sdp"], T = T) do problem
+                Convex.solve!(problem, () -> Tulip.Optimizer{T}())
             end
         end
     end
