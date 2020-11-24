292209638420d750928fc0e9d469226007e3fc4d
mtanneau <mathieu.tanneau@gmail.com>
2020-11-24 15:40:04 -0500

    Remove unused code

--- src/IPM/MPC/step.jl
+++ src/IPM/MPC/step.jl
@@ -26,9 +26,6 @@ function compute_step!(mpc::MPC{T, Tv}, params::IPMOptions{T}) where{T, Tv<:Abst
     θinv = θl .+ θu
 
     # Update regularizations
-    # mpc.regP .= clamp(sqrt(pt.μ), sqrt(eps(T)), sqrt(sqrt(eps(T))))
-    # mpc.regD .= clamp(sqrt(pt.μ), sqrt(eps(T)), sqrt(sqrt(eps(T))))
-    
     mpc.regP ./= 10
     mpc.regD ./= 10
     clamp!(mpc.regP, sqrt(eps(T)), one(T))
@@ -80,42 +77,6 @@ function compute_step!(mpc::MPC{T, Tv}, params::IPMOptions{T}) where{T, Tv<:Abst
         (-(pt.xu .* pt.zu) .+ σ * pt.μ .- Δ.xu .* Δ.zu) .* dat.uflag,
     )
     αp, αd = max_step_length_pd(pt, Δ)
-
-    # Extra corrections
-    #=ncor = 0
-    while ncor < params.BarrierCorrectionLimit && α < T(999 // 1000)
-        α_ = α
-        ncor += 1
-
-        # Compute extra-corrector
-        αc = compute_higher_corrector!(Δc, 
-            mpc, γ, 
-            hx, hy, h0,
-            Δ, α_, params.BarrierCentralityOutlierThreshold
-        )
-        if αc > α_
-            # Use corrector
-            Δ.x  .= Δc.x
-            Δ.xl .= Δc.xl
-            Δ.xu .= Δc.xu
-            Δ.y  .= Δc.y
-            Δ.zl .= Δc.zl
-            Δ.zu .= Δc.zu
-            Δ.τ   = Δc.τ
-            Δ.κ   = Δc.κ
-            α = αc
-        end
-        
-        if αc < T(11 // 10) * α_
-            break
-        end
-
-        # if (1.0 - η * αc) >= 0.9*(1.0 - η * α_)
-        #     # not enough improvement, step correcting
-        #     break
-        # end
-        
-    end =#
 
     # Update current iterate
     αp *= params.BarrierStepDampFactor
@@ -216,28 +177,6 @@ function solve_newton_system!(Δ::Point{T, Tv},
     return nothing
 end
 
-#=
-"""
-    max_step_length(x, dx)
-
-Compute the maximum value `a ≥ 0` such that `x + a*dx ≥ 0`, where `x ≥ 0`.
-"""
-function max_step_length(x::Vector{T}, dx::Vector{T}) where{T}
-    n = size(x, 1)
-    n == size(dx, 1) || throw(DimensionMismatch())
-    a = T(Inf)
-
-    #=@inbounds=# for i in Base.OneTo(n)
-        if dx[i] < zero(T)
-            if (-x[i] / dx[i]) < a
-                a = (-x[i] / dx[i])
-            end
-        end
-    end
-    return a
-end
-=#
-
 """
     max_step_length_pd(pt, δ)
 
@@ -253,101 +192,4 @@ function max_step_length_pd(pt::Point{T, Tv}, δ::Point{T, Tv}) where{T, Tv<:Abs
     αd = min(one(T), azl, azu)
 
     return αp, αd
-end
-
-
-
-"""
-    compute_higher_corrector!(Δc, mpc, γ, hx, hy, h0, Δ, α, β)
-
-Compute higher-order corrected direction.
-
-Requires the solution of one Newton system.
-
-# Arguments
-- `Δc`: Corrected search direction, modified in-place
-- `mpc`: The MPC optimizer
-- `γ`: 
-- `hx, hy, h0`: Terms obtained from the preliminary augmented system solve
-- `Δ`: Current predictor direction
-- `α`: Maximum step length in predictor direction
-- `β`: Relative threshold for centrality outliers
-"""
-function compute_higher_corrector!(Δc::Point{T, Tv},
-    mpc::MPC{T, Tv}, γ::T,
-    hx::Tv, hy::Tv, h0::T,
-    Δ::Point{T, Tv}, α::T, β::T,
-) where{T, Tv<:AbstractVector{T}}
-    error("Higher corrector not implemented for MPC")
-    # TODO: Sanity checks
-    pt = mpc.pt
-    dat = mpc.dat
-
-    # Tentative step length
-    α_ = min(one(T), T(2)*α)
-
-    # Tentative cross products
-    vl = ((pt.xl .+ α_ .* Δ.xl) .* (pt.zl .+ α_ .* Δ.zl)) .* dat.lflag
-    vu = ((pt.xu .+ α_ .* Δ.xu) .* (pt.zu .+ α_ .* Δ.zu)) .* dat.uflag
-    vt = (pt.τ   + α_  * Δ.τ)   * (pt.κ   + α_  * Δ.κ)
-
-    # Compute target cross-products
-    mu_l = β * pt.μ * γ
-    mu_u = γ * pt.μ / β
-    for i in 1:pt.n
-        dat.lflag[i] || continue
-        if vl[i] < mu_l
-            vl[i] = mu_l - vl[i]
-        elseif vl[i] > mu_u
-            vl[i] = mu_u - vl[i]
-        else
-            vl[i] = zero(T)
-        end
-    end
-    for i in 1:pt.n
-        dat.uflag[i] || continue
-        if vu[i] < mu_l
-            vu[i] = mu_l - vu[i]
-        elseif vu[i] > mu_u
-            vu[i] = mu_u - vu[i]
-        else
-            vu[i] = zero(T)
-        end
-    end
-    if vt < mu_l
-        vt = mu_l - vt
-    elseif vt > mu_u
-        vt = mu_u - vt
-    else
-        vt = zero(T)
-    end
-
-    # Shift target cross-product to satisfy `v' * e = 0`
-    δ = (sum(vl) + sum(vu) + vt) / (pt.p + 1)
-    vl .-= δ
-    vu .-= δ
-    vt  -= δ
-
-    # Compute corrector
-    @timeit mpc.timer "Newton" solve_newton_system!(Δc, mpc, hx, hy, h0,
-        # Right-hand sides
-        tzeros(Tv, pt.m), tzeros(Tv, pt.n), tzeros(Tv, pt.n), tzeros(Tv, pt.n), zero(T),
-        vl,
-        vu,
-        vt
-    )
-
-    # Update corrector
-    Δc.x  .+= Δ.x
-    Δc.xl .+= Δ.xl
-    Δc.xu .+= Δ.xu
-    Δc.y  .+= Δ.y
-    Δc.zl .+= Δ.zl
-    Δc.zu .+= Δ.zu
-    Δc.τ   += Δ.τ
-    Δc.κ   += Δ.κ
-
-    # Compute corrected step-length
-    αc = max_step_length(pt, Δc)
-    return αc
 end
