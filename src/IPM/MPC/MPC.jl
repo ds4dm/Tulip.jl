5c3903db9c81f4863da7bbd50b190f6795a3dcbe
mtanneau <mathieu.tanneau@gmail.com>
2020-12-15 18:35:04 -0500

    Cosmetic changes

--- src/IPM/MPC/MPC.jl
+++ src/IPM/MPC/MPC.jl
@@ -16,7 +16,6 @@ mutable struct MPC{T, Tv, Tb, Ta, Tk} <: AbstractIPMOptimizer{T}
     primal_status::SolutionStatus     # Primal solution status
     dual_status::SolutionStatus       # Dual   solution status
 
-
     primal_objective::T  # Primal bound: c'x
     dual_objective::T    # Dual bound: b'y + l' zl - u'zu
 
@@ -176,7 +175,7 @@ function update_solver_status!(mpc::MPC{T}, ϵp::T, ϵd::T, ϵg::T, ϵi::T) wher
     else
         mpc.dual_status = Sln_Unknown
     end
-    
+
     # Check for optimal solution
     if ρp <= ϵp && ρd <= ϵd && ρg <= ϵg
         mpc.primal_status = Sln_Optimal
@@ -184,7 +183,7 @@ function update_solver_status!(mpc::MPC{T}, ϵp::T, ϵd::T, ϵg::T, ϵi::T) wher
         mpc.solver_status = Trm_Optimal
         return nothing
     end
-    
+
     # TODO: Primal/Dual infeasibility detection
     # Check for infeasibility certificates
     if max(
@@ -273,12 +272,12 @@ function ipm_optimize!(mpc::MPC{T}, params::IPMOptions{T}) where{T}
         if params.OutputLevel > 0
             # Display log
             @printf "%4d" mpc.niter
-            
+
             # Objectives
             ϵ = dat.objsense ? one(T) : -one(T)
             @printf "  %+14.7e" ϵ * mpc.primal_objective
             @printf "  %+14.7e" ϵ * mpc.dual_objective
-            
+
             # Residuals
             @printf "  %8.2e" max(mpc.res.rp_nrm, mpc.res.rl_nrm, mpc.res.ru_nrm)
             @printf " %8.2e" mpc.res.rd_nrm
@@ -311,14 +310,14 @@ function ipm_optimize!(mpc::MPC{T}, params::IPMOptions{T}) where{T}
             || mpc.solver_status == Trm_DualInfeasible
         )
             break
-        elseif mpc.niter >= params.IterationsLimit 
+        elseif mpc.niter >= params.IterationsLimit
             mpc.solver_status = Trm_IterationLimit
             break
         elseif ttot >= params.TimeLimit
             mpc.solver_status = Trm_TimeLimit
             break
         end
-        
+
 
         # TODO: step
         # For now, include the factorization in the step function
@@ -330,7 +329,7 @@ function ipm_optimize!(mpc::MPC{T}, params::IPMOptions{T}) where{T}
             if isa(err, PosDefException) || isa(err, SingularException)
                 # Numerical trouble while computing the factorization
                 mpc.solver_status = Trm_NumericalProblem
-    
+
             elseif isa(err, OutOfMemoryError)
                 # Out of memory
                 mpc.solver_status = Trm_MemoryLimit
@@ -344,16 +343,13 @@ function ipm_optimize!(mpc::MPC{T}, params::IPMOptions{T}) where{T}
 
             break
         end
-
         mpc.niter += 1
-
     end
-    
+
     # TODO: print message based on termination status
     params.OutputLevel > 0 && println("Solver exited with status $((mpc.solver_status))")
 
     return nothing
-
 end
 
 function compute_starting_point(mpc::MPC{T}) where{T}
@@ -367,7 +363,7 @@ function compute_starting_point(mpc::MPC{T}) where{T}
     # Get initial iterate
     KKT.solve!(zeros(T, n), pt.y, mpc.kkt, false .* mpc.dat.b, mpc.dat.c)  # For y
     KKT.solve!(pt.x, zeros(T, m), mpc.kkt, mpc.dat.b, false .* mpc.dat.c)  # For x
-    
+
     # I. Recover positive primal-dual coordinates
     δx = one(T) + max(
         zero(T),
@@ -391,11 +387,11 @@ function compute_starting_point(mpc::MPC{T}) where{T}
     =#
     pt.zl .= ( z ./ (dat.lflag + dat.uflag)) .* dat.lflag
     pt.zu .= (-z ./ (dat.lflag + dat.uflag)) .* dat.uflag
-    
+
     δz = one(T) + max(zero(T), (-3 // 2) * minimum(pt.zl), (-3 // 2) * minimum(pt.zu))
     pt.zl[dat.lflag] .+= δz
     pt.zu[dat.uflag] .+= δz
-    
+
     mpc.pt.τ   = one(T)
     mpc.pt.κ   = zero(T)
 
